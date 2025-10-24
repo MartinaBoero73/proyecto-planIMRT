cef25aa7ed9e2468a150a725423364c9d8f3a5d5
MartinaBoero73 <martinaboero73@gmail.com>
2025-10-24 17:07:08 -0300

    add: agrega archivos de proyecto de spring

---
+++ src/test/java/com/planimrt/PlanImrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planimrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanImrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
