bf7879d38fafbab38780f1690ee872b196f2aec7
MartinaBoero73 <martinaboero73@gmail.com>
2025-10-29 20:05:28 -0300

    add: agrega configuracion cors

---
+++ src/main/java/com/planimrt/security/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.planimrt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        System.out.println("Configuración CORS cargada correctamente");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+
+        };
+    }
+}
+
