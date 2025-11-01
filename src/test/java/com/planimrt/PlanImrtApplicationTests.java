c90792dddc571ce4c89929db68d7dbf47f30c733
MartinaBoero73 <martinaboero73@gmail.com>
2025-11-01 18:41:08 -0300

    update: agrega perfil de test a archivos de tests

--- src/test/java/com/planimrt/PlanImrtApplicationTests.java
+++ src/test/java/com/planimrt/PlanImrtApplicationTests.java
@@ -2,8 +2,10 @@ package com.planimrt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PlanImrtApplicationTests {
 
 	@Test
